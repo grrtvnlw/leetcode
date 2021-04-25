838352c0b7f03b6bf7a5fa90c5e982ff6fa0c699
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2021-04-25 00:33:34 -0400

    Two Sum

--- twoSum.ts
+++ twoSum.ts
@@ -1,20 +1,12 @@
 function twoSum(nums: number[], target: number): number[] {
-  let firstNum = nums[0];
-  let secondNum = nums[1];
-  let result = [];
-
-  for (let i = 1; i <= nums.length; i++) {
-    if (firstNum + secondNum === target) {
-      result.push(nums.indexOf(firstNum), nums.indexOf(secondNum));
-      return result;
-    } else {
-      firstNum = secondNum;
-      secondNum = nums[i];
+  for (let i = 0; i < nums.length - 1; i++) {
+    let firstNum = nums[i];
+    for (let j = 1; j < nums.length; j++) {
+      let secondNum = nums[j];
+      if (firstNum + secondNum === target && i !== j) {
+        const result = [i, j];
+        return result;
+      }
     }
   }
 }
-
-const numbers = [2, 7, 11, 15],
-  goal = 9;
-
-console.log(twoSum(numbers, goal));
